6dcb97dd7b3c649ec96a82adf1b0ca13bc68d66b
Abílio <abilio_mg_castro@hotmail.com>
2018-12-16 22:07:50 +0000

    Update Main.java

--- ESIIProject/src/main/java/Main.java
+++ ESIIProject/src/main/java/Main.java
@@ -1,14 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        String teste;
-
-        FileManager fm = new FileManager();
-
-        fm.insertFile("C:\\Users\\Ricardo\\Desktop\\DOC.txt");
-        fm.insertFile("C:\\Users\\Ricardo\\Desktop\\DOC2.txt");
-
-        System.out.println();
+       
 
     }
 }
