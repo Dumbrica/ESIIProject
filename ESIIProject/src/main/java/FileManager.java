ef7c5ce209c76f895214ff71894e897bbee0d0c1
Abílio <abilio_mg_castro@hotmail.com>
2018-12-27 14:12:03 +0000

    Update FileManager.java

--- ESIIProject/src/main/java/FileManager.java
+++ ESIIProject/src/main/java/FileManager.java
@@ -7,37 +7,34 @@ public class FileManager {
     private String[] files;
     private static int DEFAULT_SIZE = 5;
     private int filesCount = 0;
-    private String  totalWords ="";
+    private String totalWords = "";
 
     public FileManager() {
         files = new String[DEFAULT_SIZE];
     }
 
-    public FileManager(int files_size){
+    public FileManager(int files_size) {
         files = new String[files_size];
     }
 
-    public boolean insertFile(String filePath){
+    public boolean insertFile(String filePath) {
 
         String aux;
 
         try {
-           aux = readFile(filePath);
-        }catch (IOException ex){
+            aux = readFile(filePath);
+        } catch (IOException ex) {
             return false;
         }
 
-        aux = aux.replaceAll("[0-9]","");
+        aux = removeDigits(aux);
         files[filesCount] = aux;
         totalWords = totalWords + files[filesCount];
         filesCount++;
 
         System.out.println(totalWords);
-        return  true;
+        return true;
     }
-
-
-
 
     private String readFile(String filePath) throws IOException {
 
@@ -47,4 +44,11 @@ public class FileManager {
 
         return file;
     }
+
+    public String removeDigits(String texto)
+    {
+        texto=texto.replaceAll("[0-9]", "");
+        return texto;
+    }
 }
+
