63fc545967f7a746f025344049475e4e9509978f
Ricardo <Dumbrica@hotmail.com>
2018-12-27 20:05:32 +0000

    Por favor upsource

--- src/main/java/FileManager.java
+++ src/main/java/FileManager.java
@@ -50,7 +50,7 @@ public class FileManager {
     }
 
     public void testMethod(){
-        System.out.println("Teste2222654256");
+        System.out.println("Upsource PLS");
     }
 	public String removeChars(String texto){
 		texto=texto.replaceAll("[\"\\,\\.\\?\\!\\|\\[\\]\\{\\}\\/\\;\\:\\«\\»\\<\\>\\@\\£\\€\\§\\#\\$\\%\\&\\=\\)\\(\\*\\+\\~\\^\\_\\-]","");
