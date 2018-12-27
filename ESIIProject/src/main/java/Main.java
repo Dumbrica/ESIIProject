c718661fc36f27703e3bf64399dff493fecd766c
Abílio <abilio_mg_castro@hotmail.com>
2018-12-27 13:38:28 +0000

    Revert "Testes"
    This reverts commit 7fdb779dea107176f4122b681ee5cb91cd9f3b37.

--- ESIIProject/src/main/java/Main.java
+++ ESIIProject/src/main/java/Main.java
@@ -1,17 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-<<<<<<< HEAD
-=======
-        String teste;
-
-        FileManager fm = new FileManager();
-
-        fm.insertFile("C:\\Users\\Ricardo\\Desktop\\DOC.txt");
-        fm.insertFile("C:\\Users\\Ricardo\\Desktop\\DOC2.txt");
-
-        System.out.println();
->>>>>>> parent of 6dcb97d... Update Main.java
 
     }
 }
