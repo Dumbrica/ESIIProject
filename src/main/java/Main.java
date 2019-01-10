388b483eb4ffddc34415b485aebbcf6da0cc3bad
abiliomg <abilio_mg_castro@hotmail.com>
2019-01-10 13:21:46 +0000

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        System.out.println("Insira a query");
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String query = br.readLine();
+
         FileManager teste=new FileManager(2);
         teste.insertFile("um.txt");
         teste.insertFile("dois.txt");
-        String query="um dois tres";
         teste.insertQuery(query);
         query=teste.getQuery();
 
@@ -32,21 +40,31 @@ public class Main {
             System.out.println(i);
         }*/
         grauSim=teste.orderGrauS(grauSim);
-        int opcao=2,quantFMostrar=3;
+        int opcao=0,quantFMostrar=3;
         double limite=0.2;
-        switch (opcao){
-            case 1:
-                System.out.println(teste.imprimirLCompleta(grauSim,teste.getFilesName()));
-                break;
-            case 2:
-                //fazer reader com limite entre 0 e 1
-                System.out.println(teste.imprimirLGrauLimite(grauSim,teste.getFilesName(),limite));
-                break;
-            case 3:
-                //fazer reader com numero maximo de ficheiros a mostrar >0
-                System.out.println(teste.imprimirLLimitada(grauSim,teste.getFilesName(),quantFMostrar));break;
 
-        }
+        do {
+            System.out.println("Escolha opção: 1 - Lista Completa, 2 - Lista Acima de x, 3 - Lista de x Ficheiros, 4 - Sair");
+            String read=br.readLine();
+            opcao =read.
+            switch (opcao){
+                case 1:
+                    System.out.println(teste.imprimirLCompleta(grauSim,teste.getFilesName()));
+                    break;
+                case 2:
+                    //fazer reader com limite entre 0 e 1
+                    System.out.println(teste.imprimirLGrauLimite(grauSim,teste.getFilesName(),limite));
+                    break;
+                case 3:
+                    //fazer reader com numero maximo de ficheiros a mostrar >0
+                    System.out.println(teste.imprimirLLimitada(grauSim,teste.getFilesName(),quantFMostrar));break;
+                case 0:
+                    return ;
+                default:
+            }
+        }while(true);
+
+
 
     }
 
